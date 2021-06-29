1280c1633dc011ca98a397c9829deca999547e46
Simon Kuforiji <simonkuforiji@gmail.com>
2021-06-29 14:41:48 +0100

    Deployed with Heroku and added README file

---
+++ README.md
@@ -0,0 +1,3 @@
+This is a Newsletter signup web app built with Node.js, Express.js, HTML, CSS
+I used the Mailchimp API to save the Sugn up list
+I deployed it with Heroku
--- app.js
+++ app.js
@@ -61,7 +61,10 @@ app.post("/failure", function(req, res){
     res.redirect("/");
 })
 
-const port = 3000;
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
 app.listen(port, function(){
     console.log(`Server running on port ${port}`);
 })
